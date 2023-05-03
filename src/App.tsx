d879557fbd21b3ad2550b40858abd9deae5483a0
aysuxalilli <aysuxalilli19@gmail.com>
2023-05-03 16:55:43 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,87 @@
-import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
-import Popup from './Popup';
-import Payment from './Payment';
-import Calculyator from './Calculyator';
-const App = () => {
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, StyleSheet} from 'react-native';
+
+export default function calculyator() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+
+  const add = () => {
+    const res = parseFloat(num1) + parseFloat(num2);
+    setResult(res.toString());
+  };
+
+  const subtract = () => {
+    const res = parseFloat(num1) - parseFloat(num2);
+    setResult(res.toString());
+  };
+
+  const multiply = () => {
+    const res = parseFloat(num1) * parseFloat(num2);
+    setResult(res.toString());
+  };
+
+  const divide = () => {
+    const res = parseFloat(num1) / parseFloat(num2);
+    setResult(res.toString());
+  };
+
   return (
-    <SafeAreaView style={styles.container}>
-      {/* <Popup />  */}
-      {/* <Payment /> */}
-      <Calculyator />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Kalkulyator</Text>
+      <TextInput
+        style={styles.input}
+        keyboardType="phone-pad"
+        value={num1}
+        onChangeText={setNum1}
+        placeholder="birinci ededi daxil edin"
+      />
+      <TextInput
+        style={styles.input}
+        keyboardType="phone-pad"
+        value={num2}
+        onChangeText={setNum2}
+        placeholder='ikinci ededi daxil edin'
+
+      />
+      <View style={styles.buttons}>
+        <Button title="+" onPress={add} />
+        <Button title="-" onPress={subtract} />
+        <Button title="*" onPress={multiply} />
+        <Button title="/" onPress={divide} />
+      </View>
+      <Text style={styles.result}>Result: {result}</Text>
+    </View>
   );
-};
-export default App;
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#E5E5E5',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 40,
+  },
+  input: {
+    height: 46,
+    width: '90%',
+    borderWidth: 2,
+    borderColor: 'green',
+    padding: 10,
+    marginVertical: 10,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '70%',
+    marginBottom: 20,
+  },
+  result: {
+    fontSize: 20,
+    fontWeight: 'normal',
   },
 });
